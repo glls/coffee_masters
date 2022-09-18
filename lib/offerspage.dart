ea3dc5e05903d460335b53c560e574849deb2704
George Litos <gl@cyberpunk.gr>
2022-09-18 18:38:40 +0300

    Offers page

---
+++ lib/offerspage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class OffersPage extends StatelessWidget {
+  const OffersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Offer extends StatelessWidget {
+  //properties
+  final String title;
+  final String description;
+  const Offer({Key? key, required this.title, required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
