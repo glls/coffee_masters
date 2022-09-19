787e1301ce2d026e038939536626cb5727821874
George Litos <gl@cyberpunk.gr>
2022-09-19 23:56:48 +0300

    factory constructors

--- lib/datamodel.dart
+++ lib/datamodel.dart
@@ -6,12 +6,23 @@ class Product {
   String get imageUrl =>
       "https://firtman.github.io/coffeemasters/api/images/$image";
 
+  // constructor
   Product({
     required this.id,
     required this.name,
     required this.price,
     required this.image,
   });
+
+  // factory constructor
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      price: json['price'] as double,
+      image: json['image'] as String,
+    );
+  }
 }
 
 class Category {
@@ -22,6 +33,13 @@ class Category {
     required this.name,
     required this.products,
   });
+
+  // factory constructor
+  factory Category.fromJson(Map<String, dynamic> json) {
+    var productsJson = json['products'] as Iterable<dynamic>;
+    var products = productsJson.map((p) => Product.fromJson(p)).toList();
+    return Category(name: json['name'] as String, products: products);
+  }
 }
 
 class ItemInCart {
